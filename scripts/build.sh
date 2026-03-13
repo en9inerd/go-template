4cb56a49dcb3a1e00c16fb198bb0d5f0a9b7da0f
Vladimir <18351400+en9inerd@users.noreply.github.com>
2026-03-13 10:48:52 -0400

    chore: update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ CGO_ENABLED=0
 DIST_DIR="dist"
 VERSION=${VERSION:-dev}
 
-platforms=("linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64")
+platforms=("linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64" "windows/amd64" "windows/arm64")
 
 for platform in "${platforms[@]}"
 do
